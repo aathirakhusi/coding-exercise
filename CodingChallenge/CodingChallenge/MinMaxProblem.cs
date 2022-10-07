549882dd1c1a08826ddc772da26792ac0170bfc0
Aathira <wpepele@gmail.com>
2022-10-07 19:17:11 +0300

    Min Max and Time Conversion

---
+++ CodingChallenge/CodingChallenge/MinMaxProblem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    public class MinMaxProblem
+    {
+        public static void MinMaxPblm(string[] args)
+        {
+
+            List<int> arr = new List<int> { 1,2,3,4,5};
+
+            Result.miniMaxSum(arr);
+        }
+    }
+    class Result
+    {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<int> arr)
+        {
+            long sum = 0;
+            int min = arr[0];
+            int max = arr[0];
+            for(int i =0;i<arr.Count;i++)
+            {
+                sum=sum+arr[i];
+                if (arr[i]<min)
+                {
+                    min=arr[i];
+                }
+                if(arr[i]>max)
+                {
+                    max=arr[i];
+                }
+
+            }
+            Console.WriteLine($"{sum-max} {sum-min}");
+
+        }
+
+    }
+}
+        
---
+++ CodingChallenge/CodingChallenge/TimeConversion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    public class TimeConversion
+    {
+        public static void Main(string[] args)
+        {
+
+
+            string time = "01:01:00PM";
+            var amOrPm = time.Substring(8);
+            var hourComponent = time.Substring(0, 2);
+            var remainingTimeComponent = time.Substring(2, 6);
+            if (string.Equals(amOrPm, "AM") && Convert.ToInt32(hourComponent) == 12)
+            {
+                hourComponent = "00";
+            }
+            else if (string.Equals(amOrPm, "PM"))
+            {
+                var numericHourComponent = int.Parse(hourComponent);
+                if (numericHourComponent != 12)
+                {
+                    hourComponent = Convert.ToString(numericHourComponent + 12);
+                }
+            }
+            Console.WriteLine(hourComponent + remainingTimeComponent);
+
+        }
+    }
+}
