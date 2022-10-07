4a24161a0176216d4a9ce53b372ae0f10449a8ce
Aathira <wpepele@gmail.com>
2022-10-07 19:35:28 +0300

    Adding comments

--- CodingChallenge/CodingChallenge/TimeConversion.cs
+++ CodingChallenge/CodingChallenge/TimeConversion.cs
@@ -12,10 +12,18 @@ namespace CodingChallenge
         {
 
 
-            string time = "01:01:00PM";
+            string time = "12:01:00PM";
+
+            // To get AM or PM
             var amOrPm = time.Substring(8);
+
+            // To get the hour part
             var hourComponent = time.Substring(0, 2);
+
+            // To get the remaining part after hour part as it remains same in military time.
             var remainingTimeComponent = time.Substring(2, 6);
+
+            // Only if hour is 12 then hour component needs to be set to 12 if its AM
             if (string.Equals(amOrPm, "AM") && Convert.ToInt32(hourComponent) == 12)
             {
                 hourComponent = "00";
